7933a9dc6f1780ca9510b71b7a054b2d598beee6
Victor Cazanave <victor.cazanave@gmail.com>
2020-08-11 19:57:03 +0800

    Clean default project

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // Custom rules
+    indent: ['error', 'tab'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-tabs': 'off',
   },
 };
